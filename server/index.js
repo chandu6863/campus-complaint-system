31b039ca7e873b3e146a82571ca04263e338fd8d
chandu6863 <chandubabbadhi@gmail.com>
2026-03-14 02:00:29 +0530

    Prepared code for deployment

--- server/index.js
+++ server/index.js
@@ -10,9 +10,12 @@ const complaintRoutes = require('./routes/complaints');
 const app = express();
 
 app.use(cors({
-  origin: process.env.CLIENT_URL || 'http://localhost:3000',
+  origin: [
+    'http://localhost:3000',
+    process.env.CLIENT_URL
+  ],
   credentials: true
-}));
+}))
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
