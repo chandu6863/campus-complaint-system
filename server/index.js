3600bdd8ebf294f3c5055157539b12c9581e0e84
chandu6863 <chandubabbadhi@gmail.com>
2026-03-14 09:39:35 +0530

    Updated CORS with Vercel URLs

--- server/index.js
+++ server/index.js
@@ -12,6 +12,8 @@ const app = express();
 app.use(cors({
   origin: [
     'http://localhost:3000',
+    'https://campus-complaint-system-iota.vercel.app',
+    'https://campus-complaint-system-git-main-chandu6863s-projects.vercel.app',
     process.env.CLIENT_URL
   ],
   credentials: true
